caa846a499f55e70d92fb09b96da9bf2da877d8b
Ainando Kei <ainandoo@gmail.com>
2023-01-07 22:47:44 +0700

    add: auth login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function() {
+
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function(){
+
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+    });
+
+    Route::group(['middleware' => ['auth']], function(){
+
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::resource('/absenmonitor', \App\Http\Controllers\AbsenMonitorController::class);
